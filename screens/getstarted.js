c8c69e443e31efeebfc4998513107bc1fd8c428c
Elyas Asmad <elyasasmadz@gmail.com>
2022-10-19 02:49:40 +0800

    ✅ Onboading screen update

--- screens/getstarted.js
+++ screens/getstarted.js
@@ -1,71 +1,96 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, Dimensions, Image, Pressable } from "react-native";
+import { StyleSheet, Text, Dimensions, Image, Pressable, View, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+// import SvgHiking from "../components/svgs/HikingSVG";
+import Lottie from 'lottie-react-native';
 
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+const SCREEN_HEIGHT = Dimensions.get('screen').height;
 
 const GetStarted = ({ navigation }) => {
-  return (
-    <SafeAreaView style={styles.container}>
-        <StatusBar style="dark" backgroundColor="#000" />
-        <Image
-          style={styles.background}
-          source={require("../Background/BG1.png")}
-        />
-        <Text style={styles.name}>
-          GEOTRACKER
-        </Text>
-        <Text style={styles.desc}>
-          The best view comes{"\n"}
-          after the hardest climb
-        </Text>
-        <Pressable style={styles.Pressable} onPress={() => navigation.navigate('load')}>
-          <Text style={styles.button}>Get Started</Text>
-        </Pressable>
-      </SafeAreaView>
-  )
-}
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar style="dark" backgroundColor="#000" />
+            <Image
+                style={styles.background}
+                source={require("../Background/BG1.png")}
+            />
+            <View>
+                {/* <SvgHiking /> */}
+                <Lottie
+                    source={require('../assets/lottie/65726-traveler.json')}
+                    style={{
+                        // width: SCREEN_WIDTH / 1.5,
+                        height: SCREEN_HEIGHT / 3,
+                        aspectRatio: 1,
+                    }}
+                />
+                <Text style={styles.name}>GeoTracker</Text>
+                <Text style={styles.desc}>
+                    The best view comes{"\n"}
+                    after the hardest climb
+                </Text>
+            </View>
+            <Pressable
+                style={styles.Pressable}
+                onPress={() => navigation.navigate("load")}
+                android_ripple={{
+                    color: '#aaa'
+                }}
+            >
+                <Text style={styles.button}>Get Started</Text>
+            </Pressable>
+        </SafeAreaView>
+    );
+};
 
-export default GetStarted
+export default GetStarted;
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
     },
     background: {
-      height: windowHeight,
-      width: windowWidth,
+        height: SCREEN_HEIGHT,
+        width: SCREEN_WIDTH,
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: -1
     },
     name: {
-      fontSize: 40,
-      position: "absolute",
-      color: '#eee',
-      backgroundColor: '#e5aab2',
-      top: 400,
+        fontSize: 40,
+        color: "#fff",
+        // backgroundColor: "#e5aab2",
+        fontFamily: 'Inter-Bold'
     },
     desc: {
-      position: 'absolute',
-      fontSize: 16,
-      color: '#eee',
-      top: 450,
-      marginTop: 10,
-      textAlign: 'center',
-      backgroundColor: '#f12b32',
+        fontSize: 18,
+        color: "#aaa",
+        textAlign: "left",
+        fontFamily: 'Inter-Regular'
+        // backgroundColor: "#f12b32",
     },
     Pressable: {
-      position: "absolute",
-      bottom: 45,
-      backgroundColor: "#eee",
-      paddingVertical: 7,
-      paddingHorizontal: 30,
-      borderRadius: 5,
-      elevation: 10,
+        position: "absolute",
+        bottom: 45,
+        backgroundColor: "#fff",
+        paddingVertical: 10,
+        width: SCREEN_WIDTH - 50,
+        borderRadius: 5,
+        alignItems: 'center',
     },
     button: {
-      color: '#000',
+        color: "#000",
+        fontFamily: 'Inter-Medium',
+        fontSize: 16
+    },
+    introContainer: {
+        
     }
-  });
+});
