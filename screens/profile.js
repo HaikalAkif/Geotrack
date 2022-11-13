b296bc3043bfce94ffaf117a38271cd3acf051a0
HaikalAkif <haikalakif17@gmail.com>
2022-11-14 00:35:27 +0800

    Create Profile Page

--- screens/profile.js
+++ screens/profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, ScrollView} from 'react-native';
+import {SafeAreaView, Text, ScrollView} from 'react-native';
 import Entypo from 'react-native-vector-icons/Entypo';
 
 const Profile = () => {
@@ -8,76 +8,71 @@ const Profile = () => {
 
   for (let index = 0; index < numberofcircels; index++) {
     circuls.push(
-      <View key={index}>
+      <SafeAreaView key={index}>
         {index === 0 ? (
-          <View
-            style={{
-              width: 60,
-              height: 60,
-              borderRadius: 100,
-              borderWidth: 1,
-              opacity: 0.7,
-              marginHorizontal: 5,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Entypo name="plus" style={{fontSize: 40, color: 'black'}} />
-          </View>
+          <SafeAreaView style={styles.container}>
+            <Entypo name="plus" style={styles.Gname} />
+          </SafeAreaView>
         ) : (
-          <View
-            style={{
-              width: 60,
-              height: 60,
-              borderRadius: 100,
-              backgroundColor: 'black',
-              opacity: 0.1,
-              marginHorizontal: 5,
-            }}></View>
+          <SafeAreaView style={styles.Tname}></SafeAreaView>
         )}
-      </View>,
+      </SafeAreaView>,
     );
   }
 
   return (
-    <View style={{width: '100%', height: '100%', backgroundColor: 'white'}}>
-      <View style={{width: '100%', padding: 10}}>
+    <SafeAreaView style={styles.profilePage}>
+      <SafeAreaView style={styles.profileBody}>
         <ProfileBody
-          name="Mr Peobody"
-          accountName="mr_peobody"
+          name="Haikal Akif"
+          accountName="ikool.s"
         //   profileImage={require('../../storage/images/userProfile.png')}
-          followers="3.6M"
-          following="35"
-          post="458"
+          followers="365"
+          following="17"
+          post="4"
         />
         <ProfileButtons
           id={0}
-          name="Mr Peobody"
-          accountName="mr_peobody"
+          name="Haikal Akif"
+          accountName="ikool.s"
         //   profileImage={require('../../storage/images/userProfile.png')}
         />
-      </View>
-      <View>
-        <Text
-          style={{
-            padding: 10,
-            letterSpacing: 1,
-            fontSize: 14,
-          }}>
-          Story Highlights
-        </Text>
-        <ScrollView
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          style={{
-            paddingVertical: 5,
-            paddingHorizontal: 10,
-          }}>
-          {circuls}
-        </ScrollView>
-      </View>
-      <BottomTabView />
-    </View>
+      </SafeAreaView>
+    </SafeAreaView>
   );
 };
 
 export default Profile;
+
+const styles = StyleSheet.create({
+    container: {
+      width: 60,
+      height: 60,
+      borderRadius: 100,
+      borderWidth: 1,
+      opacity: 0.7,
+      marginHorizontal: 5,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    Gname: {
+      fontSize: 40,
+      color: 'black',
+    },
+    Tname: {
+      width: 60,
+      height: 60,
+      borderRadius: 100,
+      backgroundColor: 'black',
+      opacity: 0.1,
+      marginHorizontal: 5,
+    },
+    profilePage: {
+      width: '100%',
+      height: '100%',
+      backgroundColor: 'white',
+  },
+  profileBody: {width: '100%',
+  padding: 10,
+  },
+})
