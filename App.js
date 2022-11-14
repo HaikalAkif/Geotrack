de26a6fb590424e55afcf335af6d5611e96dc532
Elyas Asmad <elyasasmadz@gmail.com>
2022-11-14 23:57:18 +0800

    ✅ Fixed indentation

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import GetStarted from "./screens/getstarted";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Load from "./screens/load";
 import Signin from "./screens/signin";
 import Home from "./screens/home";
@@ -12,23 +12,24 @@ import Feature from "./screens/feature";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <SafeAreaProvider>
-      <Stack.Navigator initialRouteName="getstarted" screenOptions={{
-        headerShown: false
-      }}>
-        <Stack.Screen name="getstarted" component={GetStarted}/>
-        <Stack.Screen name="load" component={Load}/>
-        <Stack.Screen name="signin" component={Signin}/>
-        <Stack.Screen name="home" component={Home}/>
-        <Stack.Screen name="profile" component={Profile}/>
-        <Stack.Screen name="map" component={Map}/>
-        <Stack.Screen name="feature" component={Feature}/>
-      </Stack.Navigator>
-      </SafeAreaProvider>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <SafeAreaProvider>
+                <Stack.Navigator
+                    initialRouteName="getstarted"
+                    screenOptions={{
+                        headerShown: false,
+                    }}
+                >
+                    <Stack.Screen name="getstarted" component={GetStarted} />
+                    <Stack.Screen name="load" component={Load} />
+                    <Stack.Screen name="signin" component={Signin} />
+                    <Stack.Screen name="home" component={Home} />
+                    <Stack.Screen name="profile" component={Profile} />
+                    <Stack.Screen name="map" component={Map} />
+                    <Stack.Screen name="feature" component={Feature} />
+                </Stack.Navigator>
+            </SafeAreaProvider>
+        </NavigationContainer>
+    );
 }
-
-
