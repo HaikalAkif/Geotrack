315064f0be623ae2b110a086e841ec6002ed77b3
HaikalAkif <haikalakif17@gmail.com>
2022-11-16 00:22:21 +0800

    Rename file

---
+++ screens/explore.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, Dimensions, Pressable } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from '@react-navigation/native'
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+const Feature = ({}) => {
+    return(
+        <SafeAreaView style={styles.container}>
+            <Text>
+                Sini buat explore hiking and waterfall spots
+            </Text>
+        </SafeAreaView>
+    )
+}
+
+export default Feature
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+})
