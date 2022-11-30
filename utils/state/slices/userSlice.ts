a55e63518217bb1d081081032661c866cffcbcea
Elyas Asmad <elyasasmadz@gmail.com>
2022-11-30 22:44:24 +0800

    ✅ App global state update

--- utils/state/slices/userSlice.ts
+++ utils/state/slices/userSlice.ts
@@ -2,9 +2,15 @@ import { StateCreator } from "zustand";
 
 export interface UserSlice {
     loggedIn: boolean;
+    uid: string;
+    setUID: (uid: string) => void;
     user: UserDetail;
     setUser: (user: UserDetail) => void;
     logout: () => void;
+    signInResponse: string;
+    setSignInResponse: (response: string) => void;
+    firstTimeUser: boolean;
+    setFirstTimeUser: (state: boolean) => void;
 }
 
 export interface UserDetail {
@@ -27,10 +33,16 @@ const userInitialState: UserDetail = {
 
 export const createUserSlice: StateCreator<UserSlice, [], [], UserSlice> = (set) => ({
     loggedIn: false,
+    uid: "",
+    setUID: (uid) => set((state) => ({ uid })),
     user: userInitialState,
     setUser: (user) => set((state) => ({
         loggedIn: true,
         user,
     })),
-    logout: () => set((state) => ({ ...userInitialState, loggedIn: false }))
+    logout: () => set((state) => ({ ...userInitialState, loggedIn: false })),
+    signInResponse: '',
+    setSignInResponse: (response) => set((state) => ({ signInResponse: response })),
+    firstTimeUser: true,
+    setFirstTimeUser: (userState) => set((state) => ({ firstTimeUser: userState })) 
 })
