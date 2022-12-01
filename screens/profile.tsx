a43d19a4e769a0a687846426083fad5d3d91387c
Elyas Asmad <elyasasmadz@gmail.com>
2022-12-01 20:56:26 +0800

    ✅ Screen page slight improvements

--- screens/profile.tsx
+++ screens/profile.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Text, View, ScrollView, Dimensions, StyleSheet, Image, StatusBar as RNStatusBar, Pressable } from "react-native";
+import React, { useRef, useState } from "react";
+import { Text, View, ScrollView, Dimensions, StyleSheet, Image, Pressable, StatusBar as RNStatusBar } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Feather } from '@expo/vector-icons';
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
@@ -9,14 +9,20 @@ import { GeotrackerTheme } from "../theme/GeotrackerTheme";
 import ProfilePost from "../components/profile/ProfilePost";
 import auth from '@react-native-firebase/auth'
 import { useStore } from "../utils/state/useBoundStore";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
+import { AnimatePresence, MotiView } from "moti";
+import { SharedElement } from "react-navigation-shared-element";
+import { UserPost } from "../types/UserPost";
 
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
+const SCREEN_HEIGHT = Dimensions.get("screen").height;
+const SCREEN_WIDTH = Dimensions.get("screen").width;
+
+const POPUP_HEIGHT = SCREEN_HEIGHT / 2;
+const POPUP_WIDTH = SCREEN_WIDTH - 50;
+
+const STATUSBARHEIGHT = RNStatusBar.currentHeight;
 
 type Params = NativeStackScreenProps<GeotrackerScreenParams, 'profile'>
-
-const statusbarHeight = RNStatusBar.currentHeight!;
 
 const mockProfilePostsData = [
     {
@@ -30,15 +36,45 @@ const mockProfilePostsData = [
         source: 'https://imgs.search.brave.com/hqpD-_R-0PZ0kdEJJu8b0mtz43nYISeLLGSc-9_mI5s/rs:fit:612:408:1/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vcGhvdG9z/L3Nlbmlvci1tYW4t/dHJhaWwtaGlraW5n/LWluLXRoZS1mb3Jl/c3QtYXQtc3Vuc2V0/LXBpY3R1cmUtaWQ1/OTgyMjI1MzI_az02/Jm09NTk4MjIyNTMy/JnM9NjEyeDYxMiZ3/PTAmaD0xLTF2QXBX/R2ZDVlRzNWVkZnVO/TW5rckstallVUFkw/ZHY2MVBsVW83SVdv/PQ',
         username: 'haikalakif',
         uid: 'eykxl.s'
+    },
+    {
+        id: '789',
+        source: 'https://imgs.search.brave.com/sKik5XB0c54cLTSUAn_uSE9plTgPjuVGiflZCK_9wzc/rs:fit:1200:1024:1/g:ce/aHR0cHM6Ly93YXRj/aG1lc2VlLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvVGhlLVdo/YW5naWUtaGlrZS1u/ZWFyLUdsYXNnb3ct/MTAtMTUzNngxMDI0/LmpwZw',
+        username: 'elyasasmad',
+        uid: 'elyasasmad123'
+    },
+    {
+        id: '012',
+        source: 'https://imgs.search.brave.com/hqpD-_R-0PZ0kdEJJu8b0mtz43nYISeLLGSc-9_mI5s/rs:fit:612:408:1/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vcGhvdG9z/L3Nlbmlvci1tYW4t/dHJhaWwtaGlraW5n/LWluLXRoZS1mb3Jl/c3QtYXQtc3Vuc2V0/LXBpY3R1cmUtaWQ1/OTgyMjI1MzI_az02/Jm09NTk4MjIyNTMy/JnM9NjEyeDYxMiZ3/PTAmaD0xLTF2QXBX/R2ZDVlRzNWVkZnVO/TW5rckstallVUFkw/ZHY2MVBsVW83SVdv/PQ',
+        username: 'haikalakif',
+        uid: 'eykxl.s'
     }
 ]
+
+/**
+ * Taken from tim-montague
+ * https://stackoverflow.com/a/55987576/16475345
+ */
+const formatNumber = (n: number) => {
+
+    if (n < 1e3) return n;
+    if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
+    if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
+    if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
+    if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
+};
 
 const Profile = ({ navigation, route }: Params) => {
 
     const user = useStore((state) => state.user)
 
+    // const scaleValue = useSharedValue(0)
+    const [isPopupOpen, setIsPopupOpen] = useState<UserPost | null>(null)
+
+    const [ currentImage, setCurrentImage ] = useState('')
+
     return(
-        <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
             <StatusBar style="dark" />
             <ScrollView>
                 <View style={styles.card}>
@@ -61,17 +97,19 @@ const Profile = ({ navigation, route }: Params) => {
                     </Text>
                 </View>
                 <View style={styles.follow}>
-                    <View>
+                    <View style={{ flex: 1 }}>
                         <Text style={styles.count}>2</Text>
-                        <Text>Posts</Text>
+                        <Text style={styles.profileLabel}>Posts</Text>
                     </View>
-                    <View>
-                        <Text style={styles.count}>12.2k</Text>
-                        <Text>Followers</Text>
+                    <View style={{ flex: 1 }}>
+                        <Text style={styles.count}>
+                            {formatNumber(128415715)}
+                        </Text>
+                        <Text style={styles.profileLabel}>Followers</Text>
                     </View>
-                    <View>
+                    <View style={{ flex: 1 }}>
                         <Text style={styles.count}>17</Text>
-                        <Text>Following</Text>
+                        <Text style={styles.profileLabel}>Following</Text>
                     </View>
                 </View>
                 <View style={styles.btn}>
@@ -91,12 +129,80 @@ const Profile = ({ navigation, route }: Params) => {
                 <View style={styles.pics}>
                     {
                         mockProfilePostsData.map((item, index) => (
-                            <ProfilePost {...item} key={item.id} />  
+                            <ProfilePost {...item} key={item.id} onLongPress={setIsPopupOpen} />  
                         ))
                     }
                 </View>
             </ScrollView>
-        </SafeAreaView>
+            <SharedElement
+                id='general.bg'
+                style={[ 
+                    StyleSheet.absoluteFillObject,
+                    {
+                        transform: [ { translateY: SCREEN_HEIGHT } ],
+                    }
+                ]}
+            >
+                <View style={[
+                    StyleSheet.absoluteFillObject,
+                    {
+                        borderRadius: 16,
+                        backgroundColor: '#fff',
+                    }
+                ]} />
+            </SharedElement>
+            <AnimatePresence>
+                {!!isPopupOpen ? (
+                    <>
+                        <MotiView
+                            style={{
+                                position: 'absolute',
+                                minHeight: POPUP_HEIGHT, 
+                                width: POPUP_WIDTH,
+                                top: (SCREEN_HEIGHT / 2) - (POPUP_HEIGHT / 2),
+                                zIndex: 2,
+                                left: (SCREEN_WIDTH / 2) - (POPUP_WIDTH / 2),
+                                // backgroundColor: '#9ef7ff',
+                                backgroundColor: '#fff',
+                                borderRadius: 30,
+                                overflow: 'hidden',
+                            }}
+                            from={{ transform: [ { translateY: POPUP_HEIGHT }, { scale: 0 } ] }}
+                            animate={{ transform: [ { translateY: 0 }, { scale: 1 } ] }}
+                            exit={{ transform: [ { translateY: POPUP_HEIGHT }, { scale: 0 } ] }}
+                            exitTransition={{ type: 'timing', duration: 250 }}
+                            transition={{ type: 'spring', stiffness: 500, damping: 50 }}
+                        >
+                            <Image 
+                                source={{
+                                    uri: isPopupOpen.source
+                                }}
+                                style={{
+                                    width: '100%',
+                                    aspectRatio: 1,
+                                    borderRadius: 20
+                                }}
+                            />
+                            <View style={{ marginHorizontal: 20, marginTop: 10 }}>
+                                <Text style={{ fontSize: 20, fontFamily: GeotrackerTheme.font.bold }}>{isPopupOpen.username}</Text>
+                            </View>
+                        </MotiView>
+                        <MotiView
+                            style={{
+                                ...StyleSheet.absoluteFillObject,
+                                backgroundColor: '#0005',
+                                zIndex: 1
+                            }}
+                            from={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            exitTransition={{ type: 'timing', duration: 250 }}
+                            transition={{ type: 'spring', stiffness: 100, damping: 10 }}
+                        />
+                    </>
+                ) : null}
+            </AnimatePresence>
+        </View>
     )
 }
 
@@ -120,6 +226,7 @@ const styles = StyleSheet.create({
         width: 80,
         height: 80,
         top: 125,
+        elevation: 10
         // borderWidth: 3,
         // borderColor: '#c1e1c1'
     },
@@ -131,18 +238,17 @@ const styles = StyleSheet.create({
     bio: {
         fontSize: 14,
         textAlign: 'center',
+        fontFamily: GeotrackerTheme.font.regular
     },
     follow: {
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'space-evenly',
         marginTop: 20,
-        left: 10,
     },
     count: {
-        fontSize: 16,
-        fontWeight: 'bold',
+        fontSize: 20,
+        fontFamily: GeotrackerTheme.font.bold,
         textAlign: 'center',
     },
     btn: {
@@ -184,6 +290,14 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         marginTop: 5,
+        flexWrap: 'wrap',
     },
+    profileLabel: {
+        fontFamily: GeotrackerTheme.font.regular,
+        textAlign: 'center'
+    },
+    bottomSheet: {
+        
+    }
 })
 
