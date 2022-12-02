f9c6afcc44a6f045c9c6836450d3599712ae252f
Elyas Asmad <elyasasmadz@gmail.com>
2022-12-02 23:44:25 +0800

    🚧 Tab bar update

--- components/TabBar/GTabBarComponent.tsx
+++ components/TabBar/GTabBarComponent.tsx
@@ -1,67 +1,110 @@
-import { LayoutChangeEvent, Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { BottomTabNavigationOptions } from '@react-navigation/bottom-tabs';
-import Animated, { useAnimatedStyle, withTiming } from 'react-native-reanimated'
+import {
+    LayoutChangeEvent,
+    Pressable,
+    StyleSheet,
+    Text,
+    View,
+} from "react-native";
+import React, { useRef } from "react";
+import { BottomTabNavigationOptions } from "@react-navigation/bottom-tabs";
+import Animated, {
+    useAnimatedStyle,
+    withSpring,
+    withTiming,
+} from "react-native-reanimated";
 
 type TabBarComponentProps = {
     active?: boolean;
     options: BottomTabNavigationOptions;
     onLayout: (e: LayoutChangeEvent) => void;
     onPress: () => void;
-}
+};
 
-const GTabBarComponent = ({ active, options, onLayout, onPress }: TabBarComponentProps) => {
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable)
+
+const GTabBarComponent = ({
+    active,
+    options,
+    onLayout,
+    onPress,
+}: TabBarComponentProps) => {
+    const ref = useRef(null);
 
     const animatedComponentCircleStyles = useAnimatedStyle(() => {
         return {
-          transform: [
-            {
-              scale: withTiming(active ? 1 : 0, { duration: 250 })
-            }
-          ]
-        }
-      })
-    
-      const animatedIconContainerStyles = useAnimatedStyle(() => {
+            transform: [
+                {
+                    // scale: withTiming(active ? 1 : 0, { duration: 250 }),
+                    scale: withSpring(active ? 1 : 0, { stiffness: 200, damping: 20 }),
+                },
+            ],
+        };
+    });
+
+    const animatedIconContainerStyles = useAnimatedStyle(() => {
         return {
-          opacity: withTiming(active ? 1 : 0.5, { duration: 250 })
+            // opacity: withTiming(active ? 1 : 0.5, { duration: 250 }),
+            opacity: withSpring(active ? 1 : 0.5, { stiffness: 200, damping: 20 }),
+        };
+    });
+
+    const animatedTranslateY = useAnimatedStyle(() => {
+     
+        return {
+            transform: [
+                { translateY: withSpring(active ? -30 : 0, { stiffness: 200, damping: 20 }) },
+                // { scale: withSpring(active ? 1 : 0.85, { stiffness: 200, damping: 20 }) }
+            ]
         }
-      })
-    
-      return (
-        <Pressable onPress={onPress} onLayout={onLayout} style={styles.component}>
+
+    })
+
+    return (
+        <AnimatedPressable
+            onPress={onPress}
+            onLayout={onLayout}
+            style={[styles.component, animatedTranslateY]}
+        >
             <Animated.View
                 style={[styles.componentCircle, animatedComponentCircleStyles]}
             />
-            <Animated.View style={[styles.iconContainer, animatedIconContainerStyles]}>
+            <Animated.View
+                style={[styles.iconContainer, animatedIconContainerStyles]}
+            >
                 {/* @ts-ignore */}
-                {options.tabBarIcon ? options.tabBarIcon({ ref }) : <Text>?</Text>}
+                {options.tabBarIcon ? (
+                    options.tabBarIcon({ ref })
+                ) : (
+                    <Text>?</Text>
+                )}
             </Animated.View>
-        </Pressable>
-    )
+        </AnimatedPressable>
+    );
+};
 
-}
-
-export default GTabBarComponent
+export default GTabBarComponent;
 
 const styles = StyleSheet.create({
     component: {
         height: 60,
         width: 60,
-        marginTop: -5,
+        // marginTop: -5,
+        transform: [
+            { translateY: -30 }
+        ]
     },
     componentCircle: {
         flex: 1,
         borderRadius: 30,
-        backgroundColor: 'white',
+        backgroundColor: "white",
     },
     iconContainer: {
-        position: 'absolute',
+        position: "absolute",
         top: 0,
         left: 0,
         right: 0,
         bottom: 0,
-        justifyContent: 'center',
-        alignItems: 'center'
+        justifyContent: "center",
+        alignItems: "center",
     },
-})
+});
